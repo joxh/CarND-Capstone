847f02f9e356d369bb3e2a697e8167b4a0c58309
Yipeng Wang <alanxiaoyi@gmail.com>
2017-12-25 12:22:03 -0800

    dbw: car follows track successfully
    Add lowpass filter part and brake calculation to the controller
    node. The car can successfully follow the track. Fine tuning
    needed for the lowpass and PID controllers.
    
    Bug: green beam moves further and further away from the car as
    driving on the track. Not sure if it is waypoint or controller issue.
    Need to further look into this.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -13,6 +13,7 @@ class Controller(object):
         # I just port all variables from dbw_node here
         # not sure if all of them will be used.
         self.vehicle_mass = kwargs['vehicle_mass']
+        self.fuel_capacity = kwargs['fuel_capacity']
         self.brake_deadband = kwargs['brake_deadband']
         self.decel_limit = kwargs['decel_limit']
         self.accel_limit = kwargs['accel_limit']
@@ -30,10 +31,14 @@ class Controller(object):
         # Use PID for linear and angular controll: 
         # https://discussions.udacity.com/t/solved-compute-cte-without-car-position/364383/2?u=alanxiaoyi
         # I just used some random parameters for now
+        # TODO: the parameters need to be tuned
         self.pid_ctl = PID(0.1, 0.1, 0.1, mn = self.decel_limit, mx = self.accel_limit)
 
-
-
+        # Create a lowpass filter
+        # TODO: the parameters I just use random values I copied from discussion board
+        # (https://discussions.udacity.com/t/vehicle-not-steering-what-could-be-going-on-if-waypoint-follower-emits-small-steer-values/401647/3?u=alanxiaoyi)
+        # need fine tuned later.
+        self.lp_filt = LowPassFilter(0.96, 1.0)
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
@@ -45,12 +50,34 @@ class Controller(object):
         dbw_enable = kwargs['dbw_enable']
 
         # Get the steer from yaw contoller:
-        steer = self.yaw_ctl.get_steering(target_linear.x, target_angular.x, current_velocity.twist.linear.x)
+        steer = self.yaw_ctl.get_steering(target_linear.x, target_angular.z, current_velocity.twist.linear.x)
 
         # use pid control to get throttle
         # We only need .x from twist since it is the ego frame, x is forward
         # reference: https://answers.ros.org/question/199866/turtlebot-linear-y-has-no-response/
         error = target_linear.x - current_velocity.twist.linear.x
-        throttle = self.pid_ctl.step(error, time_delta)
-        
-        return 10., 0., 0.
+        # PID get accel
+        accel = self.pid_ctl.step(error, time_delta)
+        # filter to smooth the accel
+        accel = self.lp_filt.filt(accel)
+        throttle = 0.0
+        brake = 0.0
+
+        # TODO: throttle may need to be tuned with a ratio (?)
+
+        # if we need throttle:
+        if accel > 0:
+                throttle = max(self.accel_limit, accel)
+
+        # if we need brake:
+        else:
+               brake = -self.accel_to_brake(max(self.decel_limit, accel))
+
+        return throttle, brake, steer
+
+
+    # We need to calculate brake from accel(decel)
+    # reference: https://discussions.udacity.com/t/what-is-the-range-for-the-brake-in-the-dbw-node/412339
+    def accel_to_brake(self, accel):
+        brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * accel * self.wheel_radius
+        return brake
